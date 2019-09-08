1768676a63a32beea61d54c1ff4680db03eff976
Samuel Nunoo <samuelnunoo@mail.com>
2019-09-08 16:44:30 -0700

    'custom_debug'

--- original_cjmsj/settings.py
+++ original_cjmsj/settings.py
@@ -66,7 +66,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = ['newcjmsjapp.herokuapp.com','cjmsj.org']
 
 
