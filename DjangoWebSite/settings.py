6326a2d1080b780cb92a5fdcb67065b782c9a900
Samuel Nunoo <samuelnunoo@mail.com>
2019-09-07 17:55:44 -0700

    'production'

--- DjangoWebSite/settings.py
+++ DjangoWebSite/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['*','cjmsj.herokuapp.com','cjmsj.org']
+DEBUG = False
+ALLOWED_HOSTS = ['newcjmsjapp.herokuapp.com','cjmsj.org']
 
 
 # Application definition
